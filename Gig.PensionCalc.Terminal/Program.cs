bd3c9bfc7d6327990aac588d5d07b2159523d317
iachaly <soft@chaly.pro>
2018-11-28 08:56:24 +0000

    правила всё компилится но не работает

--- Gig.PensionCalc.Terminal/Program.cs
+++ Gig.PensionCalc.Terminal/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Gig.PensionCalc.Business;
+using Gig.PensionCalc.Domain.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,27 +12,37 @@ namespace Gig.PensionCalc.Terminal
     {
         static void Main(string[] args)
         {
-            bool isman;
-            int  birth;
+            bool isman = true;
+            int  birth = 1984;
             int finish;
-            if (args[1] == null)
+            string name;
+            int age = 0;
+            bool isw = false;
+            switch (args.Length)
             {
-                isman = true;
-                if (args[0] == null)
-                {
-                    birth = 1984;
-                }
-                else
-                {
+                case 3:
+                    name = args[0];
+                    age = int.Parse(args[1]);
+                    isw = bool.Parse(args[2]);
+                    break;
+                case 2:
                     birth = int.Parse(args[0]);
-                }
+                    isman = bool.Parse(args[1]);
+                    break;
+                case 1:
+                    isman = bool.Parse(args[1]);
+                    birth = 1984;
+                    break;
+                case 0:
+                    Console.WriteLine("Мужского рода? Да - 1");
+                    isman = bool.Parse(Console.ReadLine());
+                    Console.WriteLine("Укажите год рождения");
+                    birth = int.Parse(Console.ReadLine());
+                    break;
+                default:
+                    break;
             }
-            else
-            {
-                birth = int.Parse(args[0]);
-                isman = bool.Parse(args[1]);
 
-            };
             if (isman)
             {
                 switch (birth)
@@ -87,11 +99,20 @@ namespace Gig.PensionCalc.Terminal
                         break;
                 }
             }
+
+            var man = new UserInfo();
+            man.Name = "nomatter";
+            man.Sex = isman ? Sex.Man : Sex.Woman;
+            man.BirthDay = DateTime.Today.AddYears(-age);
+
+            var calc = new Calculator();
+            var pensionInfo = calc.Calc(man);
+            var resu = $"{birth} erfedkl";
             var result = string.Format("{0}, тебе работать ещё {1}", 
                 isman,
                 (birth)
                 );
-            Console.WriteLine(result);
+            Console.WriteLine(resu);
             Console.ReadKey();
         }
     }
